55eaacb1971e9a6cb39a5d8dfd6d15d66389c362
Huminglong <2550721739@qq.com>
2024-07-11 15:23:15 +0800

    好友关注-关注和取消关注功能开发

--- src/main/java/com/hmdp/service/IFollowService.java
+++ src/main/java/com/hmdp/service/IFollowService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,19 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IFollowService extends IService<Follow> {
 
+    /**
+     * 关注用户
+     * @param followUserId
+     * @param isFollow
+     * @return
+     */
+    Result follow(Long followUserId, Boolean isFollow);
+
+
+    /**
+     * 是否关注用户
+     * @param followUserId
+     * @return
+     */
+    Result isFollow(Long followUserId);
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,17 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +20,45 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    /**
+     * 关注用户
+     *
+     * @param followUserId
+     * @param isFollow
+     * @return
+     */
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 1. 判断到底是关注还是取关
+        Long id = UserHolder.getUser().getId();
+        if (isFollow) {
+            // 2. 关注，新增
+            Follow follow = new Follow();
+            follow.setUserId(id).setFollowUserId(followUserId);
+            this.save(follow);
+        } else {
+            //3. 取关，删除
+            QueryWrapper<Follow> queryWrapper = new QueryWrapper<>();
+            queryWrapper.lambda().eq(Follow::getUserId, id).eq(Follow::getFollowUserId, followUserId);
+            this.remove(queryWrapper);
+        }
+        return Result.ok(isFollow ? "关注成功！" : "取关成功！");
+    }
+
+
+    /**
+     * 是否关注用户
+     *
+     * @param followUserId
+     * @return
+     */
+    @Override
+    public Result isFollow(Long followUserId) {
+        // 1. 判断到底是关注还是取关
+        Long id = UserHolder.getUser().getId();
+        QueryWrapper<Follow> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(Follow::getUserId, id).eq(Follow::getFollowUserId, followUserId);
+        int count = this.count(queryWrapper);
+        return Result.ok(count > 0);
+    }
 }
